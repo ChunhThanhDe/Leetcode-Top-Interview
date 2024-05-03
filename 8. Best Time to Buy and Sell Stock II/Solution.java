bcfc48edeb1fde20492905b69bd5a42d60870a01
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-05-03 10:25:17 +0700

    ✨ Best Time to Buy and Sell Stock II

---
+++ 8. Best Time to Buy and Sell Stock II/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int totalProfit = 0;
+
+        for (int i = 1; i < prices.length; i++){
+            if (prices[i] - prices[i - 1] > 0){
+                totalProfit+= (prices[i] - prices[i - 1]);
+            }
+        }
+
+        return totalProfit;
+    }
+}
