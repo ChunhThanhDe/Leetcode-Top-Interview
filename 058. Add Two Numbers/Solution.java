679aab86766cc1208ef85a89208e812a03ab1e52
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-08-08 16:33:01 +0700

    🔍 Add Two Numbers

---
+++ 058. Add Two Numbers/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        int carr = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            
+            int sum = x + y + carr;	
+         
+            carr = sum / 10;
+           
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carr != 0) {
+            curr.next = new ListNode(carr);
+        }
+
+        return dummy.next;
+    }
+}
