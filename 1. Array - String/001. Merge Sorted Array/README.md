6b0f0e43ad2c7dc13ab8909e92768723c84e8989
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-09-11 09:26:35 +0700

    🎨 88. Merge Sorted Arrays

--- 1. Array - String/001. Merge Sorted Array/README.md
+++ 1. Array - String/001. Merge Sorted Array/README.md
@@ -1,4 +1,4 @@
-## Merge Sorted Arrays
+## 88. Merge Sorted Arrays
 
 ### Description
 You are given two integer arrays `nums1` and `nums2`, sorted in non-decreasing order, and two integers `m` and `n`, representing the number of elements in `nums1` and `nums2` respectively.
--- 1. Array - String/001. Merge Sorted Array/Solution.java
+++ 1. Array - String/001. Merge Sorted Array/Solution.java
@@ -1,88 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        mergeArrays(nums1, m, nums2, n);
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        int i = m - 1; // index of the last element in the sorted of nums1
+        int j = n - 1; // index of the last element in nums2
+        int k = m + n -1; // index of the last position in nums1 affter merging
 
-        int k = m + n;
-        quickSort(nums1, 0, k - 1);
-        String result = "";
-
-        for (int i = 0; i < k; i++) {
-            result += nums1[i] + ", ";
-        }
-
-        if (result.length() > 0) {
-            result = result.substring(0, result.length() - 2);
-        }
-
-        System.out.println("[" + result + "]");
-    }
-
-    public static void mergeArrays(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = m; i < m + n; i ++) {
-            nums1[i] = nums2[i - m];
-        }
-    }
-
-    public static void quickSort(int[] nums, int low, int high) {
-        System.out.print("start quickSort ");
-        for (int i = low; i <= high; i++) {
-
-            if (i == low) {
-                System.out.print("[");
-            }
-
-            System.out.print(nums[i]);
-
-            if (i == high) {
-                System.out.print("]\n");
+        // loop until all elements from nums2 are merged into nums1
+        while (j >= 0){
+            // if nums1 has element left and the current element in nums1 is larger
+            if (i >= 0 && nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--]; // place nums1's element at position k
             } else {
-                System.out.print(", ");
+                nums1[k--] = nums2[j--] // Otherwise, place nums2's element at position k
             }
         }
-
-        if (low < high) {
-            int pivotIndex = partition(nums, low, high);
-            System.out.print("pivot Index: " + pivotIndex + "\n");
-            System.out.print("quickSort left side " + "\n");
-            quickSort(nums, low, pivotIndex - 1);
-            System.out.print("end quickSort left side " + Arrays.toString(Arrays.stream(nums).toArray()) + "\n");
-            System.out.print("quickSort right side" + "\n");
-            quickSort(nums, pivotIndex + 1, high);
-            System.out.print("end quickSort right side " + Arrays.toString(Arrays.stream(nums).toArray()) + "\n");
-        }
-    }
-
-    public static int partition(int[] nums, int low, int high) {
-        int pivot = nums[high];
-        int i = low - 1;
-
-        System.out.print("pivot value: " + pivot + "\n");
-        for (int j = low; j < high; j++) {
-            if (nums[j] <= pivot) {
-                i++;
-                System.out.print("nums[" + j + "] = " + nums[j] + " <= pivot transfer to the left side\n");
-                if (i != j) {
-                    swap(nums, i, j);
-                    System.out.print(Arrays.toString(Arrays.stream(nums).toArray()) + "\n");
-                }
-
-            } else {
-                System.out.print("nums[" + j + "] = " + nums[j] + " > pivot transfer to the right side\n");
-            }
-        }
-
-        System.out.print("swap pivot\n");
-        swap(nums, i + 1, high);
-        System.out.print("end partition " + Arrays.toString(Arrays.stream(nums).toArray()) + "\n");
-        return i + 1;
-    }
-
-    public static void swap(int[] nums, int i, int j) {
-        System.out.print("swap " + "nums[" + i + "] = " + nums[i] + " and " + "nums[" + j + "]" + " = " + nums[j] + "\n");
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
