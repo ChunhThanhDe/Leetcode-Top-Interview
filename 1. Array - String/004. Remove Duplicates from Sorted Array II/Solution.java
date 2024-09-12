5ae2bd4b9d0a2e40847b9056d67b4e9ca6ae1263
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-09-12 09:03:34 +0700

    🚫🚫 80. Remove Duplicates from Sorted Array II

--- 1. Array - String/004. Remove Duplicates from Sorted Array II/Solution.java
+++ 1. Array - String/004. Remove Duplicates from Sorted Array II/Solution.java
@@ -1,25 +1,29 @@
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
+
+        // if the array has tw0 or fewer element, return its length directly
+        // since it can't contain more than 2 duplicates
+        if (nums.lenth <= 2) return nums.length;
+
+        // 'k' is index where the next unique or allowed element will be placed
+        // Initialize 'k' = 2, allowing the first two element to be present
+        int k = 2;
+
+        // start iterating from the third element (index = 2)
+        for (int i = 2; i < nums.length; i++){
+
+            // if the current element 'nums[i]' is not the same with the element two positions before 'nums[k - 2]'
+            // it means this element can be keep (because allow at most two element duplicates)
+            if (nums[i] != nums[i - 2]){
+
+                // move the current element 'nums[i]' to the position 'k' and increment 'k'
+                nums[k++] = nums[i];
+            }
         }
 
-        int i = 0;
-        int index = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[j - 1]) {
-                index = 0;
-                i++;
-                nums[i] = nums[j];
-            } else {
-                index++;
-                if (index == 1){
-                    i++;
-                    nums[i] = nums[j];
-                }
-        }
-    }
-        return i + 1;
+
+        // return 'k' which represents the new length of the array with duplicates removed rule
+        return k;
     }
     
 }
