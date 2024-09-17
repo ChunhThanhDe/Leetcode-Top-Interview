0f3c0cd6fb3a992be4d0395f2be92d3dab0f9959
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-09-17 11:27:22 +0700

    55. Jump Game 🏃‍♂️

--- 1. Array - String/009. Jump Game/README.md
+++ 1. Array - String/009. Jump Game/README.md
@@ -1,4 +1,6 @@
-## Jump Game
+## 55. Jump Game 🏃‍♂️
+
+**Difficulty**: `Medium` - **Tags**: `Array`, `Greedy`, `Dynamic Programming`
 
 ### Description
 You are given an integer array `nums`. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
@@ -36,3 +38,25 @@ false
 
 Explanation:
 You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+---
+
+### Solution 💡
+
+The time complexity is $O(n)$, and the space complexity is $O(1)$, where $n$ is the number of elements in the array.
+
+#### Java
+
+```java
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) return false;
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+}
+```
+You can find the full `Solution.java` file [here](Solution.java).
--- 1. Array - String/009. Jump Game/Solution.java
+++ 1. Array - String/009. Jump Game/Solution.java
@@ -1,18 +1,25 @@
 public class Solution {
     public boolean canJump(int[] nums) {
+
+        // Tracks the farthest index 
         int maxReach = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            // If maxReach < i, it will not be possible to jump over i, so break
-            if (i > maxReach) {
-                break;
-            }
+        // loop through each index in the nums
+        for (int i = 0; i < nums.length; i++){
 
-            // This algorithm is to find the farthest point that can be jumped
-            maxReach = Math.max(maxReach, i + nums[i]);
+            // if the maxReach higher or equal nums.length, 
+            // it mean we can reach the last index,
             if (maxReach >= nums.length - 1) return true;
+
+            // if the current index is higher than current maxReach,
+            // we can not move forward
+            if (i > maxReach) return false;
+
+            // update maxReach if the current position + jump value is farther 
+            maxReach = Math.max(maxReach, i + nums[i]);
         }
 
-        return maxReach >= nums.length - 1;
+        // return false if can find way to reach the last index
+        return false;
     }
 }
