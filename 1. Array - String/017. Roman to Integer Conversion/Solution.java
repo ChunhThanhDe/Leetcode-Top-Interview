d1da247d8af79fdb1c6535934156e8875a73f347
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-09-24 10:26:20 +0700

    ✨

--- 1. Array - String/017. Roman to Integer Conversion/Solution.java
+++ 1. Array - String/017. Roman to Integer Conversion/Solution.java
@@ -8,19 +8,19 @@ class Solution {
         symbolValues.put('C', 100);
         symbolValues.put('D', 500);
         symbolValues.put('M', 1000);
-        
+
         int result = 0;
-        
+
         for (int i = 0; i < s.length(); i++) {
             int value = symbolValues.get(s.charAt(i));
-            
+
             if (i + 1 < s.length() && value < symbolValues.get(s.charAt(i + 1))) {
                 result -= value;
             } else {
                 result += value;
             }
         }
-        
+
         return result;
     }
 }
